2a8ea0a7d55e16890a754fc5176062394e6165d3
Katie <kab761@gmail.com>
2015-02-17 11:15:21 -0800

    temporary workaround for linked list bug

--- app/models/otp_trip.rb
+++ app/models/otp_trip.rb
@@ -2,33 +2,25 @@ class OTPTrip < Trip
 
 	def directions(legs)
 		dir_array = []
- 		legs.each { |leg| dir_array << Leg.new(leg) }
+		legs.each { |leg| dir_array << Leg.new(leg) }
 
 		rtn_array = []
 		dir_array.each_with_index do |dir, i|
-			dir.prev_leg = prev_element(dir_array, i)
-			dir.prev_leg = next_element(dir_array, i)
+			dir.prev_leg = i > 0 ? dir_array[i-1] : nil
+			# dir.next_leg = dir_array[i+1]
 			rtn_array << dir
 		end
 		rtn_array.each { |e| puts e.prev_leg, e.next_leg }
 		rtn_array
 	end
 
-	def prev_element(arr, i)
-		i > 0 ? arr[i-1] : nil
-	end
-
-	def next_element(arr, i)
-		arr[i+1] ? arr[i+1] : nil
-	end
-
-  def routes(origin, destination)
+	def routes(origin, destination)
 		time = Time.now
-    url = Rails.env.production? ? 'http://otp.seattle-a2b.com/' : 'http://localhost:8080/'
-    url += "otp/routers/default/plan?fromPlace=#{origin}&toPlace=#{destination}"
+		url = Rails.env.production? ? 'http://otp.seattle-a2b.com/' : 'http://localhost:8080/'
+		url += "otp/routers/default/plan?fromPlace=#{origin}&toPlace=#{destination}"
 		puts "0 s: finding transit routes"
-    rts = HTTParty.get(url)['plan']
+		rts = HTTParty.get(url)['plan']
 		puts "#{Time.now - time} s: done with transit routes"
 		rts
-  end
+	end
 end
